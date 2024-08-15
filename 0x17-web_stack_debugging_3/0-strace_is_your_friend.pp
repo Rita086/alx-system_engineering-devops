1505507471b035ff429a0fc9167388211ffc9823
Rita086 <gathogoritawangiru@gmail.com>
2024-08-14 18:14:05 -0700

    This file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to solve a bug in wp-setings.php
+# Puppet manifest to fix a bug in wp-setings.php
 
 exec { 'fix the php extension issue':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
--- 0x17-web_stack_debugging_3/README.md
+++ 0x17-web_stack_debugging_3/README.md
@@ -1,1 +1,1 @@
-This is my readme in this directory 0x17-web_stack_debugging_3
+This is my readme in this directory
